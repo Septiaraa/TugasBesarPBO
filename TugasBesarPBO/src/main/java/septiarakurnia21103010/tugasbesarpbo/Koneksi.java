41907301db6392cd3939a8dafd7ff6220ab5edec
Septiara Kurnia <21103010@ittelkom-pwt.ac.id>
2023-01-27 19:59:18 +0700

    Tugas Besar

---
+++ TugasBesarPBO/src/main/java/septiarakurnia21103010/tugasbesarpbo/Koneksi.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package septiarakurnia21103010.tugasbesarpbo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author admin
+ */
+public class Koneksi {
+    public static Connection connect;
+    public static Statement st;
+    public static ResultSet rs;
+    
+    public static Connection getConnection(){
+        if(connect==null){
+            try{
+                System.out.println("Membuat koneksi...");
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connect=DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/db_tubespbo",
+                        "root",
+                        "");
+                st = (Statement) connect.createStatement();
+                JOptionPane.showMessageDialog(null,"Terkoneksi");
+                System.out.println("Koneksi berhasil");
+            }catch(ClassNotFoundException | SQLException e){
+                JOptionPane.showMessageDialog(null,"Tidak Terkoneksi");
+                System.out.println("Koneksi tidak berhasil.");
+            }
+        }
+        return connect;
+    }
+}
---
+++ TugasBesarPBO/src/main/java/septiarakurnia21103010/tugasbesarpbo/TugasBesarPBO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package septiarakurnia21103010.tugasbesarpbo;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author admin
+ */
+public class TugasBesarPBO {
+
+    public static void main(String[] args) throws SQLException {
+        
+    }
+}
